e86a5b09e972b4c84372298aa162439a90cd8295
dzwfighting <yiwen7943@gail.com>
2024-04-29 20:03:22 -0400

    Initial commit - to build blog microservice user and blogpost module

---
+++ blogpost_service/src/main/java/com/evan/blogpost_service/BlogpostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.evan.blogpost_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostServiceApplication.class, args);
+	}
+
+}
